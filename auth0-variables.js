862fdd6a1a6acda4eb0d3357bd536c18b50f5f7a
goldbergjeffrey <goldbergjeffrey@yahoo.com>
2019-11-21 12:49:54 -0500

    adding auth0-variables.js

---
+++ auth0-variables.js
@@ -0,0 +1,6 @@
+
+var AUTH0_CLIENT_ID='jpremUKrjbLQzEb87zhFgcWJifs0CaeM'; 
+var AUTH0_DOMAIN='randomqliks.auth0.com'; 
+var AUTH0_CALLBACK_URL='https://goldbergjeffrey-pizza42.herokuapp.com/callback';
+var AUTH0_AUDIENCE = "https://goldbergjeffrey-pizza42";
+
