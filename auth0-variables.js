a1220867f3f28e79744d4b5f17671e17cb88304f
goldbergjeffrey <goldbergjeffrey@yahoo.com>
2019-11-21 14:29:04 -0500

    trying a different approach

---
+++ auth0-variables.js
@@ -0,0 +1,4 @@
+const AUTH0_CLIENT_ID='jpremUKrjbLQzEb87zhFgcWJifs0CaeM'; 
+const AUTH0_DOMAIN='randomqliks.auth0.com'; 
+const AUTH0_CALLBACK_URL='https://goldbergjeffrey-pizza42.herokuapp.com/callback';
+const AUTH0_AUDIENCE = "https://goldbergjeffrey-pizza42";
