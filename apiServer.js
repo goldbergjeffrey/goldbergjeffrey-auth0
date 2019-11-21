07bb065c716a6545033d134969ed4b84b125584f
goldbergjeffrey <goldbergjeffrey@yahoo.com>
2019-11-21 14:50:38 -0500

    issue with callback?

--- apiServer.js
+++ apiServer.js
@@ -9,7 +9,7 @@ const path = require('path');
 
 const AUTH0_CLIENT_ID='jpremUKrjbLQzEb87zhFgcWJifs0CaeM'; 
 const AUTH0_DOMAIN='randomqliks.auth0.com'; 
-const AUTH0_CALLBACK_URL='https://goldbergjeffrey-pizza42.herokuapp.com:4500/callback';
+const AUTH0_CALLBACK_URL='https://goldbergjeffrey-pizza42.herokuapp.com/callback';
 const AUTH0_AUDIENCE = "https://goldbergjeffrey-pizza42";
 
 const app = express();
--- auth0-variables.js
+++ auth0-variables.js
@@ -1,6 +1,6 @@
 
 const AUTH0_CLIENT_ID='jpremUKrjbLQzEb87zhFgcWJifs0CaeM'; 
 const AUTH0_DOMAIN='randomqliks.auth0.com'; 
-const AUTH0_CALLBACK_URL='https://goldbergjeffrey-pizza42.herokuapp.com:4500/callback';
+const AUTH0_CALLBACK_URL='https://goldbergjeffrey-pizza42.herokuapp.com/callback';
 const AUTH0_AUDIENCE = "https://goldbergjeffrey-pizza42";
 
