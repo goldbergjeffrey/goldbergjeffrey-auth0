c74675049edb32ed7e3599507b7b38cd3972c752
goldbergjeffrey <goldbergjeffrey@yahoo.com>
2019-11-21 12:18:23 -0500

    fixing for heroku

--- appServer.js
+++ appServer.js
@@ -8,12 +8,8 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-const hostname = 'goldbergjeffrey-pizza42.herokuapp.com';
+const hostname = '0.0.0.0';
 const port = 3000;
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
 
 const server = app.listen(process.env.PORT || 3000, hostname, () => {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env); 
